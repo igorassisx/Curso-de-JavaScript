02e9d3bb5795ba650af1f176375aed399e29f830
Igor Assis Mendes da Silva <105676837+igorassisx@users.noreply.github.com>
2022-07-04 21:10:10 -0300

    condições simples

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`A velocidade atual do carro é ${vel} km/h`)
+if(vel > 60) { //condição simples
+    console.log('Você ultrapassou a velocidade permitida, MULTADO!')
+}
+console.log('diriga sempre usando sinto de segurança')
