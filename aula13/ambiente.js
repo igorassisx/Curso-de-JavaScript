c40c6fd77a85020cdc6897877cadf8e4f16a4cf6
Igor Assis Mendes da Silva <105676837+igorassisx@users.noreply.github.com>
2022-07-21 15:14:19 -0300

    repetições em javascript

---
+++ aula13/ambiente.js
@@ -0,0 +1,23 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 10)
+
+/*
+var c = 1
+while(c <= 10) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
+
