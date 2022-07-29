2b4dca26ae99c982528aa3214abf35adfc5da0dc
Igor Assis Mendes da Silva <105676837+igorassisx@users.noreply.github.com>
2022-07-29 14:51:26 -0300

    funções em javascript

---
+++ aula16/funcao01.js
@@ -0,0 +1,10 @@
+function parimp(n) {
+    if(n%2==0) {
+        return 'par'
+    } else {
+        return 'impar'
+    }
+}
+var res = parimp(10)
+
+console.log(res)
---
+++ aula16/funcao02.js
@@ -0,0 +1,5 @@
+function soma(n1=0, n2=0) { // aqui é para não dar erro no codigo caso coloque apenas um parametro
+    return n1 + n2
+}
+
+console.log(soma(2, 5))
